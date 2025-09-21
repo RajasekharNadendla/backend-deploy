f2de674aa270f5a83f21ede57184c5530c0d1ab4
Rajasekhar <rajasekhar15052003@gmail.com>
2025-09-21 13:29:18 +0530

    chandge the fike

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "backend"{
     name = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
     instance_type = "t2.micro"
     vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
-    subnet_id = local .private_subnet_id
+    subnet_id = local.private_subnet_id
     ami = data.aws_ami.ami_info.id
     tags = merge(
     var.common_tags,
